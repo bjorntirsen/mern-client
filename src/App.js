69fefe1bbf229aa98363be7101cca5f30bf92018
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-04 10:17:05 +0200

    :sparkles: Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// We use Route in order to define the different routes of our application
+import { Route } from 'react-router-dom';
+
+// We import all the components we need in our app
+import Navbar from './components/navbar';
+import Edit from './pages/edit';
+import Create from './pages/create';
+import Landing from './pages/landing';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <Route path='/edit/:id' component={Edit} />
+      <Route path='/create' component={Create} />
+    </>
   );
-}
+};
 
 export default App;
